9fee135f9502927759c4bf9036eb837a3dc7e663
malfrine <malfy.das@gmail.com>
2017-09-22 17:30:13 -0600

    struct manip for opt

--- struct_combine.m
+++ struct_combine.m
@@ -4,6 +4,10 @@ clc
 load('type_list.mat')
 load('raw_struct.mat')
 
+fields_to_rm = {'data', 'dim', 'time', 'temp_av', 'tmv', 'cgc', 'gref',...
+    'volts', 'curr', 'v_l', 'rho','tags','matl','treat'};
+s = rmfield(s,fields_to_rm);
+
 %combine structs based on type
 count = zeros(length(type_list),1);
 for i = length(s):-1:1
@@ -13,8 +17,15 @@ for i = length(s):-1:1
             count(j) = count(j) + 1;
             t(j).s(count(j)) = s(i);
         end
-    
     end
 end
+
+%manipulate t for optimization
+
+
+
+
+
+
 
 save('comb_struct','t')
